ab7184f19d371c5bc72e919a31e99898e9bff91d
b97pla <pontus.larsson@scilifelab.se>
2012-04-12 15:31:01 +0200

    use long_term_storage for miseq to upload entire flowcell

--- nextgen/bcbio/pipeline/storage.py
+++ nextgen/bcbio/pipeline/storage.py
@@ -3,7 +3,7 @@
 from bcbio.log import logger
 from bcbio.pipeline.config_loader import load_config
 from bcbio.pipeline.transfer import remote_copy
-
+from bcbio.pipeline.toplevel import _copy_from_sequencer
 
 def long_term_storage(remote_info, config_file):
     """Securely copy files from remote directory to the storage server.
@@ -15,7 +15,8 @@ def long_term_storage(remote_info, config_file):
     config = load_config(config_file)
     logger.info("Copying run data over to remote storage: %s" % config["store_host"])
     logger.debug("The contents from AMQP for this dataset are:\n %s" % remote_info)
-    _copy_for_storage(remote_info, config)
+    #_copy_for_storage(remote_info, config)
+    _copy_from_sequencer(remote_info, config)
 
 
 def _copy_for_storage(remote_info, config):
